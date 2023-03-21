9c0f85ac4ef7ce9f353ae14f931767e56cefa721
Alex Dingal <lhexie2014@gmail.com>
2023-03-21 22:59:10 +0800

    Separate header styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,55 +1,11 @@
 import React, { useState } from 'react'
-import {
-  Collapse,
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Button,
-} from 'reactstrap'
-import styled from 'styled-components'
-import useWindowDimensions from '../hooks/useWindowDimensions'
-import CustomContainer from '../atoms/CustomContainer'
-import { ReactComponent as BurgerIcon } from '../assets/burgerIcon.svg'
-import { ReactComponent as CloseIcon } from '../assets/closeIcon.svg'
-import logo from '../assets/logo.png'
-
-const StyledButton = styled(Button)`
-  display: none;
-  background-color: transparent;
-  border: none;
-  padding: 0;
-  outline: none;
-  &:focus {
-    box-shadow: none;
-  }
-  &:hover,
-  &:active {
-    background-color: transparent !important;
-    border: none;
-  }
-  @media screen and (max-width: 768px) {
-    display: block;
-  }
-`
-
-const StyledNavLink = styled(NavLink)`
-  position: relative;
-  font-weight: 600;
-  &::before {
-    content: '';
-    width: 0%;
-    height: 1px;
-    position: absolute;
-    bottom: 3px;
-    background-color: #031d44;
-    transition: 300ms;
-  }
-  &:hover::before {
-    width: 80%;
-  }
-`
+import { Collapse, Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap'
+import useWindowDimensions from '../../hooks/useWindowDimensions'
+import CustomContainer from '../../atoms/CustomContainer'
+import { ReactComponent as BurgerIcon } from '../../assets/burgerIcon.svg'
+import { ReactComponent as CloseIcon } from '../../assets/closeIcon.svg'
+import logo from '../../assets/logo.png'
+import { StyledButton, StyledNavLink } from './style'
 
 function Header() {
   const links = ['Home', 'Services', 'About', 'Portfolio', 'Blog', 'Contact']
---
+++ src/components/Header/style.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+import { NavLink, Button } from 'reactstrap'
+
+export const StyledButton = styled(Button)`
+  display: none;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  outline: none;
+  &:focus {
+    box-shadow: none;
+  }
+  &:hover,
+  &:active {
+    background-color: transparent !important;
+    border: none;
+  }
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`
+
+export const StyledNavLink = styled(NavLink)`
+  position: relative;
+  font-weight: 600;
+  &::before {
+    content: '';
+    width: 0%;
+    height: 1px;
+    position: absolute;
+    bottom: 3px;
+    background-color: #031d44;
+    transition: 300ms;
+  }
+  &:hover::before {
+    width: 80%;
+  }
+  @media (min-width: 1201px) {
+    font-size: 1.125rem;
+    padding: 8px 12px !important;
+  }
+`
